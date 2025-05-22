3a6ecf504f46c3fc311da4026ffbeafc33d4c405
Yalson-Shrestha <yalsonshrestha100@gmail.com>
2025-05-22 19:25:39 +0545

    Convert to PWA

---
+++ app/static/sw.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'budgetmate-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/bootstrap.min.css',
+    '/static/css/all.min.css',
+    '/static/manifest.json',
+    '/static/icons/android-chrome-192x192.png',
+    '/static/icons/android-chrome-512x512.png',
+    '/static/icons/apple-touch-icon.png',
+    '/static/icons/favicon-32x32.png',
+    '/static/icons/favicon-16x16.png',
+    '/static/icons/favicon.ico'
+];
+
+// Install service worker and cache assets
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                // Clone the request
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    response => {
+                        // Check if valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        // Clone the response
+                        const responseToCache = response.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+}); 
